2d743971bc863dd328a9111f2088968b959fecba
aks74uk <1220816148@qq.com>
2026-06-25 03:42:27 +0800

    Update rankings data

--- rankings_data.js
+++ rankings_data.js
@@ -1,6 +1,6 @@
 window.MR_DATA = {
-  "updateTime": "2026-06-24T18:42:27.542Z",
-  "updateTimeCN": "2026/6/25 02:42:27",
+  "updateTime": "2026-06-24T19:42:27.251Z",
+  "updateTimeCN": "2026/6/25 03:42:27",
   "total": 0,
   "items": []
 };
